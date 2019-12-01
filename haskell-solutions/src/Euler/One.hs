4582146d840a18e7cda356dfad11f87db99a9abe
Nathan Flynn <n8nflynn@gmail.com>
2019-12-01 16:40:37 +0000

    chore: add solution for problem 2

--- haskell-solutions/src/Euler/One.hs
+++ haskell-solutions/src/Euler/One.hs
@@ -1,6 +1,8 @@
 module Euler.One
 (
-    sumMultiples3or5
+    sumMultiples3or5,
+    sumFibEven,
+    fibonacci
 ) where
 
 
@@ -10,3 +12,19 @@ sumMultiples3or5 :: Int -> Int
 sumMultiples3or5 num = 
     let isMultiple a = (a `mod` 3 == 0) || (a `mod` 5 == 0)
     in sum [x | x <- [0..num-1], isMultiple x]
+
+
+-- Problem 2:
+-- By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+sumFibEven :: Int -> Int
+sumFibEven limit =
+    let fibNumbers = takeWhile (<limit) fibInfinite 
+    in sum [x | x <- fibNumbers, even x]
+
+fibonacci :: Int -> [Int]
+fibonacci n = take (n+1) $ fibInfinite
+
+fibInfinite :: [Int]
+fibInfinite = 
+    let fibInner a b = a: fibInner b (a+b)
+    in fibInner 0 1
