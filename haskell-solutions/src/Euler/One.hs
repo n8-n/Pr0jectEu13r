485c88cbd94c4b45393d25d88012a6c355922079
Nathan Flynn <n8nflynn@gmail.com>
2019-12-03 10:29:27 +0000

    chore: add solution for problem 3

--- haskell-solutions/src/Euler/One.hs
+++ haskell-solutions/src/Euler/One.hs
@@ -2,7 +2,10 @@ module Euler.One
 (
     sumMultiples3or5,
     sumFibEven,
-    fibonacci
+    fibonacci,
+    isPrime,
+    primesInfinite,
+    primeFactors
 ) where
 
 
@@ -12,6 +15,7 @@ sumMultiples3or5 :: Int -> Int
 sumMultiples3or5 num = 
     let isMultiple a = (a `mod` 3 == 0) || (a `mod` 5 == 0)
     in sum [x | x <- [0..num-1], isMultiple x]
+
 
 
 -- Problem 2:
@@ -28,3 +32,29 @@ fibInfinite :: [Int]
 fibInfinite = 
     let fibInner a b = a: fibInner b (a+b)
     in fibInner 0 1
+
+
+
+-- Problem 3:
+-- What is the largest prime factor of the number 600851475143?
+-- https://www.mathsisfun.com/prime-factorization.html
+isPrime :: Int -> Bool
+isPrime n = 
+    let greaterThanHalf x = (fromIntegral x) > (fromIntegral n/2)
+        primeIterate i
+            | greaterThanHalf i = True
+            | n `mod` i == 0    = False
+            | otherwise         = primeIterate (i+1)
+    in primeIterate 2
+
+primesInfinite :: [Int]
+primesInfinite = filter isPrime [2..]
+
+-- get last of this for answer
+primeFactors :: Int -> [Int]
+primeFactors n = 
+    let modN x   = n `mod` x == 0
+        factor   = head $ filter modN primesInfinite
+        quotient = n `div` factor
+        recurse  = factor: primeFactors quotient
+    in (if n == factor then [factor] else recurse)
