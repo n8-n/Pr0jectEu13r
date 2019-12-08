d05dc36ce7d39ad2e035f42bbc971a053780641c
Nathan Flynn <n8nflynn@gmail.com>
2019-12-08 14:58:39 +0000

    refactor: move problem description to top of file

--- haskell-solutions/src/Euler/Problems/Group1To50/P1.hs
+++ haskell-solutions/src/Euler/Problems/Group1To50/P1.hs
@@ -1,9 +1,11 @@
+-- Problem 1: 
+-- If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
 module Euler.Problems.Group1To50.P1 (
     sumMultiples3or5
 ) where
 
--- Problem 1: 
--- If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
 sumMultiples3or5 :: Int -> Int
 sumMultiples3or5 num = 
     let isMultiple a = (a `mod` 3 == 0) || (a `mod` 5 == 0)
--- haskell-solutions/src/Euler/Problems/Group1To50/P2.hs
+++ haskell-solutions/src/Euler/Problems/Group1To50/P2.hs
@@ -1,11 +1,13 @@
+-- Problem 2:
+-- By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
 module Euler.Problems.Group1To50.P2 (
     sumFibEven
 ) where
 
 import Euler.Utils.Math.Fibonacci
 
--- Problem 2:
--- By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
 sumFibEven :: Int -> Int
 sumFibEven limit =
     let fibNumbers = takeWhile (<limit) fibonacciInfinite 
