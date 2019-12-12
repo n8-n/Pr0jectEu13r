0de0d0ef8b466d3ed45bab936efb28361b99c16f
Nathan Flynn <n8nflynn@gmail.com>
2019-12-12 20:51:24 +0000

    chore: add solution for problem 6

--- haskell-solutions/src/Euler/Utils/Math/Multiple.hs
+++ haskell-solutions/src/Euler/Utils/Math/Multiple.hs
@@ -4,14 +4,17 @@ module Euler.Utils.Math.Multiple (
     lcmOfList
 ) where
 
+-- Euclid's Algorithm
 gcd' :: Int -> Int -> Int
 gcd' a 0 = a 
-gcd' a b = gcd b (a `mod` b)
+gcd' a b = gcd' b (a `mod` b)
 
+-- lcm(a,b) = a*b / gcd(a,b)
 lcm' :: Int -> Int -> Int
 lcm' 0 0 = 0
 lcm' a b = quot (a * b) (gcd' a b)
 
+-- lcm(a,b,c) = lcm(a, lcm(b,c))
 lcmOfList :: [Int] -> Int
 lcmOfList []     = 1
 lcmOfList (n:ns) = lcm' n $ lcmOfList ns
